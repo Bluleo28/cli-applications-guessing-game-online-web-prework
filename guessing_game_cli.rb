d5f75d5f637377eb6f8b4dc9906533e898b1ea0f
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 20:36:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,3 @@
 expect { run_guessing_game } . to output(/Goobye!/) .to_stdout
 { run_guessing_game } .to output(/Sorry! The computer guessed 6./) .to_stdout
 { run_guessing_game } .to output(/You guessed the correct number!/) .to_stdout
-
